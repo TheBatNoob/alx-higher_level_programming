21e50b9793e97a97f86274bfcac4710d7dc61397
TheBatNoob <amlabusamuel@gmail.com>
2022-07-06 23:08:36 +0100

    struck

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+Module 5-base_geometry
+Define class BaseGeometry
+"""
+class BaseGeometry:
+    """ Empty class """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+Define class BaseGeometry
+"""
+class BaseGeometry:
+    """ Empty class """
+    def area(self):
+        raise Exception("area() is not implemented")
